e5e0fba7f8ad12d79b2b8b4368fae47f56189a4b
Nicolo3487 <129038473+Nicolo3487@users.noreply.github.com>
2023-04-15 17:42:35 +0200

    es5s3

--- m1/s3/g5/es5/js/D10.js
+++ m1/s3/g5/es5/js/D10.js
@@ -542,9 +542,3 @@ function addCss(){
 /* ESERCIZIO 29
   Crea una funzione chiamata "isItPrime" che riceve un numero come parametro e ritorna true se il numero fornito è un numero primo.
 */
-
-
-
-
-
-
