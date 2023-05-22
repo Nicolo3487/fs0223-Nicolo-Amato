a86c7eb3a491d604cedf8a78dd4a6f2edbbd6437
Nicolo3487 <129038473+Nicolo3487@users.noreply.github.com>
2023-05-22 17:33:33 +0200

    es1u3

---
+++ unit-3/s1/g1/lez/ts basi/script.ts
@@ -0,0 +1,26 @@
+console.log('Running');
+
+let nome:string = 'Mario';
+let anni:number = 30;
+let boolean:boolean = false;
+let array:any[] = [];  //any = ogni tipo di dato
+
+
+let arrayString:string[]   = ['stringa'];
+let arrayNumber:number[] = [21,20];
+
+nome = 'Marco';
+let vuota:undefined | number;
+vuota = 0;
+
+
+let salutoVar:Function = ():void => {
+    console.log('Hi');
+}
+
+
+
+function saluto():void{
+    console.log('Hi')
+}
+saluto();
