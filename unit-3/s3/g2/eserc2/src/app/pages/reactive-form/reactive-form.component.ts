0790606914ae27b4b1d328ad3548dba053cb398b
Nicolo3487 <129038473+Nicolo3487@users.noreply.github.com>
2023-06-06 18:51:11 +0200

    ese2s3u3

--- unit-3/s3/g2/eserc2/src/app/pages/reactive-form/reactive-form.component.ts
+++ unit-3/s3/g2/eserc2/src/app/pages/reactive-form/reactive-form.component.ts
@@ -13,6 +13,28 @@ export class ReactiveFormComponent {
 
   constructor (private fb: FormBuilder){}
 
-    ngOnInit() {
-      this.form = this.fb.group({})
-}
+    ngOnInit():void {
+      this.form = this.fb.group({
+          name: [],
+          nickname: [],
+          powers: this.fb.array([]),
+          enemy: [],
+          planet: [],
+          weakness: this.fb.control([])
+      })
+
+      this.form.statusChanges.subscribe(status => console.log(status));
+      this.form.valueChanges.subscribe(value => console.log(value));
+    }
+    addPower(){
+      let control = new FormControl(null);
+      (this.form.get('powers') as FormArray).push(control);
+    }
+    getPowers(){
+      return (this.form.get('powers') as FormArray).controls;
+    }
+    submit(){
+      console.log(this.form.value);
+    }
+    
+  }
